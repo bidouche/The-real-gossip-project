01366d9cf8a57ff38a87706a697a533e8759e112
bidouche <alexetiennelev@gmail.com>
2018-10-31 17:17:26 +0100

    jusqu'au commentaire

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -10,6 +10,27 @@ class GossipsController < ApplicationController
 	def create
 	@gossip = Gossip.create(title: params[:gossip][:title], content: params[:gossip][:content])
 		puts params[:gossip].inspect
+		redirect_to gossip_path(@gossip.id)
+	end
+
+	def show
+	@gossip = Gossip.find(params[:id])
+	end
+
+	def edit
+		@gossip = Gossip.find(params[:id])
+	end
+
+	def update
+		@gossip = Gossip.update(title: params[:gossip][:title], content: params[:gossip][:content])
+		redirect_to root_path
+	end
+	def destroy
+		puts params.inspect
+		@gossip = Gossip.find(params[:id])
+		puts @gossip
+		@gossip.destroy
+		redirect_to root_path
 	end
 
 end
